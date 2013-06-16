5e9ff5c8b9a15f1bc9a044eb730b9ab20fc3335b
Amar Zubcevic <amar.zubcevic@gmail.com>
2013-06-17 00:20:10 +0200

    added preload
    Added code for preloading images even though it's not part of tutorial
    1, but will be coverd in tutorial 2, because if it is missing you have
    to refresh the browser to se the images drawn to the canvas.

--- Tutorial 1 - Dragon of Bosnia - Basic HTML, Canvas and EaselJS/javascript/main.js
+++ Tutorial 1 - Dragon of Bosnia - Basic HTML, Canvas and EaselJS/javascript/main.js
@@ -3,6 +3,26 @@ function init() {
 	canvas = document.getElementById("tutorialCanvas");
 	stage = new createjs.Stage(canvas);
 
+	//THIS CODE IS FOR IMAGE PRELOADING IT IS NOT PART OF TUTORIAL 1
+	//WE WILL LEARN HOW TO THIS PROPERLY IN THE NEXT TUTORIAL
+	//IT IS HERE SO YOU DON'T HAVE TO REFRESH YOUR BROWSER TO SE THE IMAGES ON THE CANVAS
+	var image = new Image();
+	image.src = "images/background.jpg";
+	image.onload = function() {
+	stage.update();
+	};
+	var image2 = new Image();
+	image2.src = "images/trees.jpg";
+	image2.onload = function() {
+	stage.update();
+	};
+	var image3 = new Image();
+	image3.src = "images/ground.jpg";
+	image3.onload = function() {
+	stage.update();
+	};
+	//END OF PRELOAD PART THE REST IS TUTORIAL 1
+
 	//adding the background image
 	var backgroundImage = new createjs.Bitmap("images/background.jpg");
 	stage.addChild(backgroundImage);
